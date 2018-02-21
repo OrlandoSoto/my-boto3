e04b8cf1ee5b1351b9dd759ed010159431971eda
Orlando <Orlando.Soto-Pastor@cfpb.gov>
2018-02-20 21:36:28 -0500

    Add more utilities

--- my-boto.py
+++ my-boto.py
@@ -2,6 +2,8 @@ import utility
 import boto3
 
 my_bucket_name = 'orlando-bucket'
+my_bucket_config = {'LocationConstraint': 'us-east-2'}
+upload_filename = 'uploads/sample.txt'
 
 # List all buckets by name
 utility.print_s3_buckets()
@@ -11,10 +13,20 @@ s3 = boto3.client('s3')
 
 # check if the bucket already exists before creating a bucket
 if(utility.does_bucket_exist(my_bucket_name)):
-	print(my_bucket_name + ' Already exists')
+    print(my_bucket_name + ' Already exists')
+    print('Skipping bucket creation')
 else:
-	# Create new bucket 
-	print('Creating bucket: ' + my_bucket_name) 
-	s3.create_bucket(Bucket=my_bucket_name, CreateBucketConfiguration={'LocationConstraint': 'us-east-2'})
+    # Create new bucket
+    print('Creating bucket: ' + my_bucket_name)
+    s3.create_bucket(Bucket=my_bucket_name, CreateBucketConfiguration=my_bucket_config)
 
+# check if the bucket already exists before uploading a file
+if(utility.does_bucket_exist(my_bucket_name)):
+    s3.upload_file(upload_filename, my_bucket_name, upload_filename)
+    print(upload_filename + ' uploaded to ' + my_bucket_name)
+else:
+    print(my_bucket_name + ' Does NOT exist')
 
+# Print the contents of all buckets
+print('Bucket contents: ')
+utility.print_all_bucket_contents()
--- utility.py
+++ utility.py
@@ -1,22 +1,33 @@
 import boto3
 
+
 def print_s3_buckets():
 
-	# Create an S3 client
-	s3 = boto3.client('s3')
+    # Create an S3 resource
+    s3 = boto3.resource('s3')
 
-	# Call S3 to list current buckets
-	response = s3.list_buckets()
+    # Print out the bucket list
+    for bucket in s3.buckets.all():
+        print('Bucket name: ' + bucket.name)
 
-	# Get a list of all bucket names from the response
-	buckets = [bucket['Name'] for bucket in response['Buckets']]
 
-	# Print out the bucket list
-	print("Bucket List: %s" % buckets)
+
 
 def does_bucket_exist(bucket_name):
-	s3 = boto3.resource('s3')
-	if(s3.Bucket(bucket_name) in s3.buckets.all()):
-		return	True
-	else:
-		return False
+    # Create an S3 resource
+    s3 = boto3.resource('s3')
+
+    # Check if the bucket name exists
+    if(s3.Bucket(bucket_name) in s3.buckets.all()):
+        return True
+    else:
+        return False
+
+
+def print_all_bucket_contents():
+    # Create an S3 resource
+    s3 = boto3.resource('s3')
+
+    for bucket in s3.buckets.all():
+        for key in bucket.objects.all():
+            print(key.key)
