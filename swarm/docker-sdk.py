9803bbc1a29e4aece6121c072f3275726ce82aa4
EC2 Default User <ec2-user@ip-172-31-34-217.us-east-2.compute.internal>
2019-03-22 02:11:10 +0000

    Update provisioning

--- swarm/docker-sdk.py
+++ swarm/docker-sdk.py
@@ -1,6 +1,8 @@
+import os
 import docker
 client = docker.from_env()
 
 container =client.swarm.init()
 
+os.system('docker swarm join-token manager')
 print container
--- swarm/provisioning.sh
+++ swarm/provisioning.sh
@@ -2,9 +2,11 @@
 echo 'Installing Docker'
 yum update -y
 yum -y install git
+yum -y install python-pip
 amazon-linux-extras install docker
 service docker start
 usermod -a -G docker ec2-user
 docker info
 
+pip install docker 
 
